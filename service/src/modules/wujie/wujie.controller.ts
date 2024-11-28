6cf4fa1e6271e1a2b75fb3111badbfc1013c968b
luke <>
2024-11-28 21:59:14 +0800

    fix: fiux

--- service/src/modules/wujie/wujie.controller.ts
+++ service/src/modules/wujie/wujie.controller.ts
@@ -1,4 +1,3 @@
-
 import { SuperAuthGuard } from '@/common/auth/superAuth.guard';
 import { WujieService } from './wujie.service';
 import { JwtAuthGuard } from '@/common/auth/jwtAuth.guard';
@@ -8,12 +7,12 @@ import axios from 'axios';
 import { Request, Response } from 'express';
 import { GetDrawListDto } from './dto/getDrawList.dto';
 import { AdminAuthGuard } from '@/common/auth/adminAuth.guard';
-import { getAuthorization } from '@/common/utils'
+import { getAuthorization } from '@/common/utils';
 
 @ApiTags('wujie')
 @Controller('wujie')
 export class WujieController {
-  constructor(private readonly wujieService: WujieService) { }
+  constructor(private readonly wujieService: WujieService) {}
 
   @Get('getModelInfo')
   @ApiOperation({ summary: '获取作画模型列表' })
@@ -21,23 +20,30 @@ export class WujieController {
   @ApiBearerAuth()
   async getModelInfo() {
     // console.log('***********')
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     // console.log('authorization', authorization)
     try {
-      const { data: { code, data }, status } = await axios.get('https://gate.wujieai.net/wj-open/v1/ai/model_base_infos', {
+      const {
+        data: { code, data },
+        status,
+      } = await axios.get('https://gate.wujieai.net/wj-open/v1/ai/model_base_infos', {
         headers: {
-          "Content-Type": "application/json",
-          "Authorization": `${authorization}`
-        }
-      })
-      console.log('data', data, parseInt(code))
+          'Content-Type': 'application/json',
+          Authorization: `${authorization}`,
+        },
+      });
+      console.log('data', data, parseInt(code));
       if (status !== 200) {
-        throw new HttpException('获取模型列表失败', parseInt(code))
+        throw new HttpException('获取模型列表失败', parseInt(code));
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e.message)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '获取作画模型列表' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -46,26 +52,33 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async createWithDefaultAI(@Body() params: any) {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'post',
         url: 'https://gate.wujieai.net/wj-open/v1/ai/create',
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
+          Authorization: `${authorization}`,
         },
-        data: JSON.stringify(params)
+        data: JSON.stringify(params),
       };
-      const { data: { code, data }, status } = await axios(config)
-      console.log('data', data, code)
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
+      console.log('data', data, code);
       if (status !== 200) {
-        throw new HttpException('创建默认作画失败', parseInt(code))
+        throw new HttpException('创建默认作画失败', parseInt(code));
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '创建默认作画' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -74,29 +87,39 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async createWithMJ(@Body() params: any) {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'post',
         url: 'https://gate.wujieai.net/wj-open/v1/ai/create/midjourney',
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
+          Authorization: `${authorization}`,
         },
-        data: params
+        data: params,
       };
-      const { data: { code, data }, status } = await axios(config)
-      console.log('data', data, code)
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
+      console.log('data', data, code);
       if (status !== 200) {
-        throw new HttpException({
-          desc: '创建MJ作画失败',
-          code: parseInt(code)
-        }, 500)
+        throw new HttpException(
+          {
+            desc: '创建MJ作画失败',
+            code: parseInt(code),
+          },
+          500,
+        );
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '创建MJ作画失败' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -105,25 +128,32 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async getResourseStyleModule() {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'get',
         url: 'https://gate.wujieai.net/wj-open/v1/ai/default_resource_style_model',
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
-        }
+          Authorization: `${authorization}`,
+        },
       };
-      const { data: { code, data }, status } = await axios(config)
-      console.log('data', data, code)
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
+      console.log('data', data, code);
       if (status !== 200) {
-        throw new HttpException('获取作画风格模型资源失败', parseInt(code))
+        throw new HttpException('获取作画风格模型资源失败', parseInt(code));
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '获取作画风格模型资源' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -132,25 +162,32 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async getResourseModule(@Query('model') model: string) {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'get',
         url: `https://gate.wujieai.net/wj-open/v1/ai/default_resource?model=${model}`,
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
-        }
+          Authorization: `${authorization}`,
+        },
       };
-      const { data: { code, data }, status } = await axios(config)
-      console.log('data', data, code)
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
+      console.log('data', data, code);
       if (status !== 200) {
-        throw new HttpException('获取作画模型预设资源失败', parseInt(code))
+        throw new HttpException('获取作画模型预设资源失败', parseInt(code));
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '获取作画模型预设资源' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -159,25 +196,32 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async getDrawTaskInfo(@Query('key') key: string) {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'get',
         url: `https://gate.wujieai.net/wj-open/v1/ai/info?key=${key}`,
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
-        }
-      }
-      const { data: { code, data }, status } = await axios(config)
+          Authorization: `${authorization}`,
+        },
+      };
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
       if (status !== 200) {
-        throw new HttpException('获取单个作画任务信息失败', parseInt(code))
+        throw new HttpException('获取单个作画任务信息失败', parseInt(code));
       }
-      console.log('data', data)
-      return data
+      console.log('data', data);
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '获取单个作画任务信息' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -186,26 +230,33 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async batchGetDrawTaskInfo(@Body() keys: string[]) {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'post',
         url: 'https://gate.wujieai.net/wj-open/v1/ai/generating_info',
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
+          Authorization: `${authorization}`,
         },
-        data: keys
+        data: keys,
       };
-      const { data: { code, data }, status } = await axios(config)
-      console.log('data', data)
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
+      console.log('data', data);
       if (status !== 200) {
-        throw new HttpException('批量获取作画任务信息失败', parseInt(code))
+        throw new HttpException('批量获取作画任务信息失败', parseInt(code));
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '批量获取作画任务信息' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 
@@ -214,27 +265,34 @@ export class WujieController {
   // @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   async getDrawList(@Body() body: GetDrawListDto) {
-    const authorization = getAuthorization()
+    const authorization = getAuthorization();
     try {
       const config = {
         method: 'post',
         url: 'https://gate.wujieai.net/wj-open/v1/ai/artwork/history',
         headers: {
           'Content-Type': 'application/json',
-          "Authorization": `${authorization}`
+          Authorization: `${authorization}`,
         },
-        data: body
+        data: body,
       };
-      const { data: { code, data }, status } = await axios(config)
+      const {
+        data: { code, data },
+        status,
+      } = await axios(config);
 
-      console.log('data', data)
+      console.log('data', data);
       if (status !== 200) {
-        throw new HttpException('获取所有历史作画任务列表失败', parseInt(code))
+        throw new HttpException('获取所有历史作画任务列表失败', parseInt(code));
       }
-      return data
+      return data;
     } catch (e) {
-      console.log('error', e)
-      throw e
+      console.log('error.response.data', e.response.data);
+      console.log('error.config.headers', e.config.headers);
+      console.log('error.response.status', e.response.status);
+      console.log('error.response.statusText', e.response.statusText);
+      console.log('error.response.headers', e.response.headers);
+      throw new HttpException({ desc: '获取所有历史作画任务列表' + e.response.data.message, code: e.response.data.code }, e.response.status);
     }
   }
 }
