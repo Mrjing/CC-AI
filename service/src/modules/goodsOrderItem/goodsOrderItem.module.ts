c4113c2c0519567e8a2c4dbb9e117b55d7c68310
luke <>
2025-01-20 17:24:13 +0800

    fix: order debug

--- service/src/modules/goodsOrderItem/goodsOrderItem.module.ts
+++ service/src/modules/goodsOrderItem/goodsOrderItem.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GoodsOrderItemService } from './goodsOrderItem.service';
 import { GoodsOrderItemEntity } from './goodsOrderItem.entity';
+import { GoodsOrderEntity } from '../goodsOrder/goodsOrder.entity';
+import { GoodsEntity } from '../goods/goods.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GoodsOrderItemEntity])],
+  imports: [TypeOrmModule.forFeature([GoodsOrderItemEntity, GoodsOrderEntity, GoodsEntity])],
   providers: [GoodsOrderItemService],
 })
-export class GoodsOrderItemModule {}
+export class GoodsOrderItemModule { }
